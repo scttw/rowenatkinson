432da5ac059c58d59107a74b1b6b9ffd6d48fa8d
Rowen Atkinson <rowen.atkinson@gmail.com>
2013-08-27 20:02:31 +1000

    remove timepickerield

--- timepickerfield/_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('TIMEPICKERFIELD_MODULE', 'timepickerfield');
-
-if (basename(dirname(__FILE__)) != TIMEPICKERFIELD_MODULE) {
-	throw new Exception(TIMEPICKERFIELD_MODULE . ' module not installed in correct directory');
-}
